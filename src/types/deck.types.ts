86c60153fd1f440ed50b51b73ad8523c51e99715
artu <artuon@gmail.com>
2025-06-14 11:32:54 +0200

    Refactor imports for consistency and add IDeckWithCards interface to deck types

--- src/types/deck.types.ts
+++ src/types/deck.types.ts
@@ -1,3 +1,5 @@
+import type {ICard} from './card.types';
+
 export interface IDeck {
   id: string;
   topic: string;
@@ -6,4 +8,8 @@ export interface IDeck {
   languageTo: string;
   amount: number;
   promptToAiAgent: string;
+}
+
+export interface IDeckWithCards extends IDeck {
+    cards: ICard[];
 }
