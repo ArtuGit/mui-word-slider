b363f5445cad6e141f93353073997729dd632375
artu <artuon@gmail.com>
2025-06-19 20:04:00 +0200

    Feat: add example JSON for card data; enhance user understanding of card structure

---
+++ src/constants/cards.json.example.ts
@@ -0,0 +1,29 @@
+export const CARDS_JSON_EXAMPLE = `[
+  {
+    "id": "1",
+    "sourceLanguage": "Polish",
+    "targetLanguage": "English",
+    "sourceWord": "Dzień dobry",
+    "targetWord": "Good morning / Good day",
+    "pronunciation": "/d͡ʑɛɲ ˈdɔbrɨ/",
+    "remark": "Formal greeting used until afternoon"
+  },
+  {
+    "id": "2",
+    "sourceLanguage": "Polish",
+    "targetLanguage": "English",
+    "sourceWord": "Do widzenia",
+    "targetWord": "Goodbye",
+    "pronunciation": "/dɔ viˈd͡zɛɲa/",
+    "remark": "Formal farewell, literally 'until seeing'"
+  },
+  {
+    "id": "3",
+    "sourceLanguage": "Polish",
+    "targetLanguage": "English",
+    "sourceWord": "Cześć",
+    "targetWord": "Hi / Hello / Bye (informal)",
+    "pronunciation": "/t͡ʂɛɕt͡ɕ/",
+    "remark": "Informal greeting, also used for goodbye"
+  },
+] `;
